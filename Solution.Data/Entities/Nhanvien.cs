a8dcca6d26641e20d46507778d1eea2f91e7baba
Mona Lisa <phuongnvph15343@fpt.edu.vn>
2021-11-15 15:36:56 +0700

    anh xem ho em form 1 vói ạ

---
+++ Solution.Data/Entities/Nhanvien.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Solution.Data.Entities;
+
+namespace SellingPhone.Aplication.Entities
+{
+    [Table("NhanViens")]
+    public class Nhanvien
+    {
+        [Key]
+        public int IdNhanVien { get; set; }
+        public string MaNV { get; set; }
+        public int IDRole { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Diachi { get; set; }
+        public string Phonenumber { get; set; }
+        public byte[] Hinhanh { get; set; }
+        public bool Sex { get; set; }
+        public virtual Role Roles { get; set; }
+        public virtual ICollection<HoaDon> HoaDons { get; set; }
+
+    }
+}
